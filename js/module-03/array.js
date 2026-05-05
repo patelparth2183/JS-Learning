97047d8aff73af447395c142c78cc0de8292ac16
parthpotenza <parth.patel@potenzaglobalsolutions.com>
2026-05-06 00:14:54 +0530

    learning about forEach and map

--- js/module-03/array.js
+++ js/module-03/array.js
@@ -12,3 +12,21 @@ console.log("Unshift is", arr);
 
 arr.shift();
 console.log("Shift is", arr);
+
+console.log("break"); ///////////////////////////////
+
+const arr2 = [10, 20, 30, 40, 50];
+
+const arrReturn = arr2.forEach(function (item, index) {
+    console.log(item, "=>", index);
+})
+
+console.log("break"); ///////////////////////////////
+
+const arr3 = [10, 20, 30, 40, 50];
+
+const map = arr3.map(function (item, index) {
+    return item*item;
+})
+
+console.log(map);
