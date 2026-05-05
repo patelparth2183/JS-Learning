b4059db9c2243363f4b0f182e2692c15180e5b85
patelparth2183 <patelparth2183@gmail.com>
2026-05-05 22:38:06 +0530

    array.js file is created

---
+++ js/module-03/array.js
@@ -0,0 +1,14 @@
+let arr = [10, 20, 30, 40, 50];
+console.log(arr);
+
+arr.push(100, 200);
+console.log("Push is", arr);
+
+arr.pop();
+console.log("Pop is", arr);
+
+arr.unshift(5, 15);
+console.log("Unshift is", arr);
+
+arr.shift();
+console.log("Shift is", arr);
