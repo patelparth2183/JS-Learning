65a91f7b208d294f0ad8541ac59ae3ceb45553e9
parthpotenza <parth.patel@potenzaglobalsolutions.com>
2026-05-11 00:47:30 +0530

    shift array3.js content to array.js

--- js/module-03/array.js
+++ js/module-03/array.js
@@ -14,3 +14,20 @@ arr.shift();
 console.log("Shift is", arr);
 
 console.log("break"); ///////////////////////////////
+
+// Sort
+const arr2 = ["Apple", "Mango", "Banana", "Orange"];
+console.log(arr2.sort());
+
+const fruits = ["Apple", "Mango", "Banana", "Orange"];
+// console.log(fruits.toSorted());
+// console.log(fruits);
+
+const num = [2,98,54,56,95,72,10];
+console.log(num.sort(function (a,b) {return a-b}));
+
+const minimumNumber = Math.min(...num);
+console.log(minimumNumber);
+
+console.log(fruits.sort());
+console.log(fruits.reverse());
